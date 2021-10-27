aaa159d5a66881388d892623df33c78c4c6c777c
Denver-Meadows <meadows8200@gmail.com>
2021-10-27 14:16:07 -0400

    basic styling for infobar complete

--- src/components/TopInfoBar.js
+++ src/components/TopInfoBar.js
@@ -1,7 +1,26 @@
+import { BsBox } from "react-icons/bs";
+import { IoMdNotificationsOutline } from "react-icons/io";
+import { FiSettings } from "react-icons/fi";
+
 function TopInfoBar() {
   return (
     <div className="dashboard-content-info-bar">
-      <h2>Inventory</h2>
+      <div className="logo">
+        <h2>
+          <BsBox /> Inventory+
+        </h2>
+      </div>
+      <div className="user-info">
+        <div className="user-info-notify user-info-item">
+          <IoMdNotificationsOutline />
+        </div>
+        <div className="user-info-login user-info-item">
+          <button>login</button>
+        </div>
+        <div className="user-info-settings user-info-item">
+          <FiSettings />
+        </div>
+      </div>
     </div>
   );
 }
