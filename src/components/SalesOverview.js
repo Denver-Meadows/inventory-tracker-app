1aba1b586e90b23d580696e72e83f21d6edb1907
Denver-Meadows <meadows8200@gmail.com>
2021-10-28 13:15:58 -0400

    Sales overview basic styling complete

--- src/components/SalesOverview.js
+++ src/components/SalesOverview.js
@@ -1,7 +1,54 @@
+import {
+  FcSalesPerformance,
+  FcBullish,
+  FcBearish,
+  FcMoneyTransfer,
+} from "react-icons/fc";
+
 function SalesOverview() {
   return (
     <div className="dashboard-content-sales">
-      <h3>Sales Info</h3>
+      <div className="sales-overview">
+        <h4>Sales Overview</h4>
+        <div className="sales-overview-items">
+          <div className="sales-overview-items-item">
+            <div className="sales-overview-item-icon">
+              <FcSalesPerformance />
+            </div>{" "}
+            <div className="sales-overview-item-stats">
+              <p>Total Sales</p>
+              <h2>$786</h2>
+            </div>
+          </div>
+          <div className="sales-overview-items-item">
+            <div className="sales-overview-item-icon">
+              <FcBullish />
+            </div>
+            <div className="sales-overview-item-stats">
+              <p>Revenue</p>
+              <h2>$7384</h2>
+            </div>
+          </div>
+          <div className="sales-overview-items-item">
+            <div className="sales-overview-item-icon">
+              <FcBearish />
+            </div>
+            <div className="sales-overview-item-stats">
+              <p>Cost</p>
+              <h2>$5384</h2>
+            </div>
+          </div>
+          <div className="sales-overview-items-item">
+            <div className="sales-overview-item-icon">
+              <FcMoneyTransfer />
+            </div>
+            <div className="sales-overview-item-stats">
+              <p>Profit</p>
+              <h2>$500</h2>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
