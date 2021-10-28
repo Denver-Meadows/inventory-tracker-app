df3dcd17331e833fd98281b7151c6762e004dfb3
Denver-Meadows <meadows8200@gmail.com>
2021-10-28 09:52:18 -0400

    basic styling for side-nav

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,11 +1,31 @@
+import { MdOutlineDashboard } from "react-icons/md";
+import { BsBox, BsCartCheck, BsPeople } from "react-icons/bs";
+import { BiBuildings } from "react-icons/bi";
+
 function SideNav() {
   return (
     <div className="dashboard-content-side-nav">
-      <ul>
-        <li>Dashboard</li>
-        <li>Inventory</li>
-        <li>Customers</li>
-        <li>Suppliers</li>
+      <ul className="side-nav">
+        <li className="side-nav-item">
+          {" "}
+          <MdOutlineDashboard /> Dashboard
+        </li>
+        <li className="side-nav-item">
+          {" "}
+          <BsBox /> Inventory
+        </li>
+        <li className="side-nav-item">
+          {" "}
+          <BsCartCheck /> Purchase
+        </li>
+        <li className="side-nav-item">
+          {" "}
+          <BsPeople /> Customers
+        </li>
+        <li className="side-nav-item">
+          {" "}
+          <BiBuildings /> Suppliers
+        </li>
       </ul>
     </div>
   );
