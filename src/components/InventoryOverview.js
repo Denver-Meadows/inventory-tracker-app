0d7d3d70921055a410840a1ffcd0ee02fe0d4037
Denver-Meadows <meadows8200@gmail.com>
2021-10-30 13:44:51 -0400

    Ready for ProductDetails

--- src/components/InventoryOverview.js
+++ src/components/InventoryOverview.js
@@ -1,14 +1,13 @@
 import InventorySummary from "./InventorySummary";
 import NumOfUsers from "./NumOfUsers";
+import ProductDetails from "./ProductDetails";
 
 function InventoryOverview() {
   return (
     <main className="dashboard-content-inventory-overview">
       <div className="inventory-overview">
         <InventorySummary />
-        <article className="inventory-overview-article">
-          <h2>Product Details</h2>
-        </article>
+        <ProductDetails />
         <NumOfUsers />
       </div>
     </main>
---
+++ src/components/ProductDetails.js
@@ -0,0 +1,11 @@
+import { FcComboChart, FcShipped } from "react-icons/fc";
+
+function ProductDetails() {
+  return (
+    <article className="inventory-overview-article">
+      <h3>Product Details</h3>
+    </article>
+  );
+}
+
+export default ProductDetails;
