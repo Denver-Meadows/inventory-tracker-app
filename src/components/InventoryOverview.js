f313d0c859f7d4122ca82625eaf0060d106aadd2
Denver-Meadows <meadows8200@gmail.com>
2021-10-30 13:38:28 -0400

    numUsers basic styling

--- src/components/InventoryOverview.js
+++ src/components/InventoryOverview.js
@@ -1,30 +1,15 @@
-import { FcComboChart, FcShipped } from "react-icons/fc";
+import InventorySummary from "./InventorySummary";
+import NumOfUsers from "./NumOfUsers";
 
 function InventoryOverview() {
   return (
     <main className="dashboard-content-inventory-overview">
       <div className="inventory-overview">
-        <article className="inventory-overview-article">
-          <h2>Inventory Summary</h2>
-          <div className="inventory-overview-article-content">
-            <div className="inventory-overview-article-content-summary">
-              <FcComboChart className="inventory-icon" />
-              <p>Quantity on Hand</p>
-              <h3>245</h3>
-            </div>
-            <div className="inventory-overview-article-content-summary">
-              <FcShipped className="inventory-icon" />
-              <p>Will be Received</p>
-              <h3>30</h3>
-            </div>
-          </div>
-        </article>
+        <InventorySummary />
         <article className="inventory-overview-article">
           <h2>Product Details</h2>
         </article>
-        <article className="inventory-overview-article">
-          <h2>No. of Users</h2>
-        </article>
+        <NumOfUsers />
       </div>
     </main>
   );
