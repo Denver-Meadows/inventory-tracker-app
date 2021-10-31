fc3fde54d24750da253405d961b5858bef882728
Denver-Meadows <meadows8200@gmail.com>
2021-10-31 12:37:38 -0400

    ProductDetails styled

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -4,6 +4,20 @@ function ProductDetails() {
   return (
     <article className="inventory-overview-article">
       <h3>Product Details</h3>
+      <div className="product-details">
+        <div className="product-details-item">
+          <p>Low Stock Items</p>
+          <p>30</p>
+        </div>
+        <div className="product-details-item">
+          <p>Item Group</p>
+          <p>14</p>
+        </div>
+        <div className="product-details-item">
+          <p>No. of Items</p>
+          <p>104</p>
+        </div>
+      </div>
     </article>
   );
 }
