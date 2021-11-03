bed670d69afe2cc5d9cd43a1f62518e6805c2547
Denver-Meadows <meadows8200@gmail.com>
2021-11-03 15:27:04 -0400

    Basic inventory page ready

--- src/components/DashboardLink.js
+++ src/components/DashboardLink.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const DashboardLink = () => {
   return (
-    <div>
+    <div className="dashboard-link">
       <Link to="/">Back to Dashboard</Link>
     </div>
   );
--- src/pages/Inventory.js
+++ src/pages/Inventory.js
@@ -1,12 +1,19 @@
 import React from "react";
 import DashboardLink from "../components/DashboardLink";
+import TopInfoBar from "../components/TopInfoBar";
+import SideNav from "../components/SideNav";
 
 const Inventory = () => {
   return (
-    <div>
-      <h1>Inventory Page</h1>
-      <DashboardLink />
-    </div>
+    <main className="dashboard">
+      <div className="inventory-page">
+        <TopInfoBar />
+        <SideNav />
+        <div className="inventory-page-review">
+          <h1>review inventory</h1>
+        </div>
+      </div>
+    </main>
   );
 };
 
