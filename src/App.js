5ae2b5455cc18686a1fc62f94c596baedf5fe7b9
Denver-Meadows <meadows8200@gmail.com>
2021-11-03 09:38:13 -0400

    Basic ErrorPage routing working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { Switch, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
+import ErrorPage from "./pages/ErrorPage";
 
 function App() {
   return (
     <Switch>
       <Route path="/" exact>
         <Dashboard />
+      </Route>
+      <Route path="*">
+        <ErrorPage />
       </Route>
     </Switch>
   );
---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,9 @@
+function ErrorPage() {
+  return (
+    <main className="dashboard">
+      <h1>Sorry, wrong page</h1>
+    </main>
+  );
+}
+
+export default ErrorPage;
